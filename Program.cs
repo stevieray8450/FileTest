b730cf61f088f3c7c7a46f61f2a2bc1ca5a3e296
Stephen Boniface <stevieray8450@gmail.com>
2018-02-21 20:54:01 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace FileTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] strings = { "Test1", "Test2", "Test3" };
+            string path = "test.txt";
+
+            if(File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            using (var fileStream = File.Create(path))
+            {
+                using (var fileWriter = new StreamWriter(fileStream))
+                {
+                    foreach (var s in strings)
+                    {
+                        fileWriter.WriteLine(s);
+                    }
+                }
+            }
+
+            using (var fileReader = new StreamReader(path)) {
+                var lines = fileReader.ReadToEnd();
+            }
+        }
+    }
+}
